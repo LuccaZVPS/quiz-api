56b118cff0d58a050158e8404885d37ee9c097eb
Lucca <luccazvps@gmail.com>
2022-12-27 14:34:40 -0300

    test: ensure signUp controller return bad request if no password and email  is provided

--- tests/presentation/signup.spec.ts
+++ tests/presentation/signup.spec.ts
@@ -23,4 +23,26 @@ describe("Signup Controller", () => {
     expect((await sut.handle(fakeData)).statusCode).toBe(400);
     expect((await sut.handle(fakeData)).body).toBe("Missing name");
   });
+  test("should return 400 status code if no email is provided", async () => {
+    const { sut } = makeSut();
+    const fakeData = {
+      body: {
+        name: "any",
+        password: "any",
+      },
+    };
+    expect((await sut.handle(fakeData)).statusCode).toBe(400);
+    expect((await sut.handle(fakeData)).body).toBe("Missing email");
+  });
+  test("should return 400 status code if no password is provided", async () => {
+    const { sut } = makeSut();
+    const fakeData = {
+      body: {
+        email: "any",
+        name: "any",
+      },
+    };
+    expect((await sut.handle(fakeData)).statusCode).toBe(400);
+    expect((await sut.handle(fakeData)).body).toBe("Missing password");
+  });
 });

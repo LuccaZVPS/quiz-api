d1cce36bfdf7d96e22861c4f16891844c3fe4d22
Lucca <luccazvps@gmail.com>
2022-12-27 15:38:19 -0300

    feat: ensure signUp controller return bad request if invalid password  is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -3,19 +3,27 @@ import { Controller } from "../protocols/controller";
 import { httpResponse } from "../protocols/http";
 export class SignUpController implements Controller {
   async handle(httpRequest: any): Promise<httpResponse> {
-    if (!httpRequest.body) {
-      return { body: "Missing body", statusCode: 400 };
-    }
-    const fields = ["name", "email", "password"];
-    for (var field of fields) {
-      if (!httpRequest.body[field]) {
-        return { body: `Missing ${field}`, statusCode: 400 };
+    try {
+      if (!httpRequest.body) {
+        return { body: "Missing body", statusCode: 400 };
       }
+      const fields = ["name", "email", "password"];
+      for (var field of fields) {
+        if (!httpRequest.body[field]) {
+          return { body: `Missing ${field}`, statusCode: 400 };
+        }
+      }
+      const { name, email, password } = httpRequest.body as AddAccountModel;
+      if (name.length < 3 || name.length > 16) {
+        return { body: `Invalid name`, statusCode: 400 };
+      }
+      if (password.length < 8 || password.length > 22) {
+        return { body: `Invalid password`, statusCode: 400 };
+      }
+
+      return { body: `Account created`, statusCode: 201 };
+    } catch {
+      return { body: `unknown error`, statusCode: 500 };
     }
-    const { name, email, password } = httpRequest.body as AddAccountModel;
-    if (name.length < 3 || name.length > 16) {
-      return { body: `Invalid name`, statusCode: 400 };
-    }
-    throw new Error();
   }
 }

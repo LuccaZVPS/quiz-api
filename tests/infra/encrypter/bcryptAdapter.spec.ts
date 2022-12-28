a7e73cc6a0194c6b73b8833cc5249e9e55e1d5d5
Lucca <luccazvps@gmail.com>
2022-12-28 12:34:06 -0300

    test: ensure bcryptAdapter return a hash

--- tests/infra/encrypter/bcryptAdapter.spec.ts
+++ tests/infra/encrypter/bcryptAdapter.spec.ts
@@ -12,4 +12,13 @@ describe("BcryptAdapter", () => {
     bcryptAdapter.encrypt("password");
     expect(spy).toHaveBeenCalledWith("password");
   });
+
+  test("should return a hash", () => {
+    const { bcryptAdapter } = sut();
+    const spy = jest.spyOn(bcrypt, "hashSync").mockImplementationOnce(() => {
+      return "hash";
+    });
+    const hash = bcryptAdapter.encrypt("password");
+    expect(hash).toBe("hash");
+  });
 });

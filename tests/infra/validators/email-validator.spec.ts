e87950391263d80dfb889ddce3b437c205c02569
Lucca <luccazvps@gmail.com>
2022-12-28 11:12:00 -0300

    test: ensure EmailValidatorAdpter throws if isEmail throws

--- tests/infra/validators/email-validator.spec.ts
+++ tests/infra/validators/email-validator.spec.ts
@@ -1,14 +1,21 @@
 import { EmailValidatorAdapter } from "../../../src/infra/validators/email-validator";
 import validator from "validator";
 describe("EmailValidatorAdapter", () => {
-  const emailValidator = new EmailValidatorAdapter();
+  const sut = () => {
+    return {
+      emailValidator: new EmailValidatorAdapter(),
+    };
+  };
   test("should call isEmail method is with correct value", () => {
+    const { emailValidator } = sut();
     const email = "any@gmail.com";
     const spy = jest.spyOn(validator, "isEmail");
     emailValidator.validate(email);
     expect(spy).toHaveBeenCalledWith(email);
   });
   test("should return the shame value of isEmail", () => {
+    const { emailValidator } = sut();
+
     const email = "any@gmail.com";
     var spy = jest.spyOn(validator, "isEmail").mockImplementationOnce(() => {
       return false;
@@ -21,5 +28,16 @@ describe("EmailValidatorAdapter", () => {
 
     isEmail = emailValidator.validate(email);
     expect(isEmail).toBe(true);
+  });
+  test("should throw error if isEmail throws error", () => {
+    const { emailValidator } = sut();
+
+    const email = "any@gmail.com";
+    jest.spyOn(validator, "isEmail").mockImplementationOnce(() => {
+      throw new Error();
+    });
+    expect(() => {
+      emailValidator.validate(email);
+    }).toThrow();
   });
 });

6b218948d412a38914250ff2abafb731bc91034c
Lucca <luccazvps@gmail.com>
2022-12-28 12:04:10 -0300

    test: ensure addAccount call repository with correct value

--- tests/data/add-account.spec.ts
+++ tests/data/add-account.spec.ts
@@ -1,7 +1,17 @@
 import { AddAccount } from "../../src/data/useCases/add-account";
 import { Encrypter } from "../../src/data/protocols/encrypter";
+import { AddAccountRepository } from "../../src/data/protocols/addAccountRepository";
+import { Account } from "@/domain/models/account";
 
 describe("Name of the group", () => {
+  const MakeAddAccountRepositoryStub = () => {
+    class AddAccountRepositoryStub implements AddAccountRepository {
+      async add(account: Account): Promise<boolean> {
+        return true;
+      }
+    }
+    return new AddAccountRepositoryStub();
+  };
   const makeEncrypterStub = () => {
     class EncrypterStub implements Encrypter {
       encrypt(password: string): string {
@@ -12,11 +22,13 @@ describe("Name of the group", () => {
   };
   const makeSut = () => {
     const encrypterStub = makeEncrypterStub();
-    const addAccount = new AddAccount(encrypterStub);
+    const addAccountRepositoryStub = MakeAddAccountRepositoryStub();
+    const addAccount = new AddAccount(encrypterStub, addAccountRepositoryStub);
 
     return {
       encrypterStub,
       addAccount,
+      addAccountRepositoryStub,
     };
   };
   test("should call Encrypter with correct value", async () => {
@@ -28,5 +40,20 @@ describe("Name of the group", () => {
       password: "any_password",
     });
     expect(spy).toHaveBeenCalledWith("any_password");
+  });
+
+  test("should call AddAccountRepository with correct values", async () => {
+    const { addAccount, addAccountRepositoryStub } = makeSut();
+    const spy = jest.spyOn(addAccountRepositoryStub, "add");
+    await addAccount.add({
+      name: "any_name",
+      email: "any@gmail.com",
+      password: "12345678",
+    });
+    expect(spy).toHaveBeenCalledWith({
+      name: "any_name",
+      email: "any@gmail.com",
+      password: "valid_hash",
+    });
   });
 });

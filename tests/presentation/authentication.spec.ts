a02c4f1fec23b809010e9190185eb53b81b38fc4
Lucca <luccazvps@gmail.com>
2022-12-29 10:46:49 -0300

    test: ensure auth controller return bad request if invalid data is provided

--- tests/presentation/authentication.spec.ts
+++ tests/presentation/authentication.spec.ts
@@ -50,7 +50,7 @@ describe("Authentication Controller", () => {
       error: "password not provided",
     });
   });
-  test("should return call EmailValidator with correct value", async () => {
+  test("should call EmailValidator with correct value", async () => {
     const { sut, emailValidator } = makeSut();
     const fakeData = {
       body: {
@@ -61,5 +61,35 @@ describe("Authentication Controller", () => {
     const spy = jest.spyOn(emailValidator, "validate");
     await sut.handle(fakeData);
     expect(spy).toHaveBeenCalledWith(fakeData.body.email);
+  });
+
+  test("should return bad request if EmailValidator return false", async () => {
+    const { sut, emailValidator } = makeSut();
+    const fakeData = {
+      body: {
+        email: "invalid",
+        password: "12345678",
+      },
+    };
+    const spy = jest
+      .spyOn(emailValidator, "validate")
+      .mockImplementationOnce(() => {
+        return false;
+      });
+    const response = await sut.handle(fakeData);
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toEqual({ error: "invalid email" });
+  });
+  test("should return bad request if invalid password is provided", async () => {
+    const { sut, emailValidator } = makeSut();
+    const fakeData = {
+      body: {
+        email: "correct@gmail.com",
+        password: "invalid",
+      },
+    };
+    const response = await sut.handle(fakeData);
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toEqual({ error: "invalid password" });
   });
 });

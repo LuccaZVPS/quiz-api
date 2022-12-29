112ae32a3bc6a03e3bdd769d90ab49f827887c3e
Lucca <luccazvps@gmail.com>
2022-12-29 13:22:06 -0300

    feat: ensure auth  controller return 500 if jwtCreate throws an error

--- src/presentation/controllers/auth.ts
+++ src/presentation/controllers/auth.ts
@@ -1,4 +1,9 @@
-import { badRequest, notFound, ok } from "../helpers/http-helpers";
+import {
+  badRequest,
+  internalError,
+  notFound,
+  ok,
+} from "../helpers/http-helpers";
 import { Controller } from "../protocols/controller";
 import { CreateJWT } from "../protocols/create-jwt";
 import { EmailValidator } from "../protocols/email-validator";
@@ -12,40 +17,44 @@ export class AuthController implements Controller {
     private createJWT: CreateJWT
   ) {}
   async handle(response: httpRequest): Promise<httpResponse> {
-    if (!response.body) {
-      return badRequest(`body not provided`);
-    }
-    const fields = ["email", "password"];
-    for (var field of fields) {
-      if (!response.body[field]) {
-        return badRequest(`${field} not provided`);
+    try {
+      if (!response.body) {
+        return badRequest(`body not provided`);
       }
-    }
-    const { email, password } = response.body;
-    const isEmail = this.emailValidator.validate(email);
-    if (!isEmail) {
-      return badRequest("invalid email");
-    }
-    if (
-      typeof password !== "string" ||
-      password.length < 8 ||
-      password.length > 22
-    ) {
-      return badRequest("invalid password");
-    }
-    const emailExist = await this.findByEmail.verify(email);
-    if (!emailExist) {
-      return notFound("email or password incorrect");
-    }
-    if (emailExist.password !== password) {
-      return notFound("email or password incorrect");
-    }
-    const jwtToken = await this.createJWT.create({
-      email: emailExist.email,
-      name: emailExist.name,
-      id: emailExist.id,
-    });
+      const fields = ["email", "password"];
+      for (var field of fields) {
+        if (!response.body[field]) {
+          return badRequest(`${field} not provided`);
+        }
+      }
+      const { email, password } = response.body;
+      const isEmail = this.emailValidator.validate(email);
+      if (!isEmail) {
+        return badRequest("invalid email");
+      }
+      if (
+        typeof password !== "string" ||
+        password.length < 8 ||
+        password.length > 22
+      ) {
+        return badRequest("invalid password");
+      }
+      const emailExist = await this.findByEmail.verify(email);
+      if (!emailExist) {
+        return notFound("email or password incorrect");
+      }
+      if (emailExist.password !== password) {
+        return notFound("email or password incorrect");
+      }
+      const jwtToken = await this.createJWT.create({
+        email: emailExist.email,
+        name: emailExist.name,
+        id: emailExist.id,
+      });
 
-    return ok();
+      return ok();
+    } catch {
+      return internalError("unknown error");
+    }
   }
 }

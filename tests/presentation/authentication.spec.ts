1b6f107eb7cce1bfd0092b88dbc2682c3d733593
Lucca <luccazvps@gmail.com>
2022-12-29 10:04:13 -0300

    test: ensure auth controller return 400 if no body is provided

--- tests/presentation/authentication.spec.ts
+++ tests/presentation/authentication.spec.ts
@@ -5,6 +5,14 @@ describe("Authentication Controller", () => {
       sut: new AuthController(),
     };
   };
+  test("should return bad request if no body is provided", async () => {
+    const { sut } = makeSut();
+    const fakeDate = {};
+    expect((await sut.handle(fakeDate)).statusCode).toBe(400);
+    expect((await sut.handle(fakeDate)).body).toEqual({
+      error: "body not provided",
+    });
+  });
   test("should return bad request if no email is provided", async () => {
     const { sut } = makeSut();
     const fakeDate = {
@@ -15,6 +23,19 @@ describe("Authentication Controller", () => {
     expect((await sut.handle(fakeDate)).statusCode).toBe(400);
     expect((await sut.handle(fakeDate)).body).toEqual({
       error: "email not provided",
+    });
+  });
+
+  test("should return bad request if no password is provided", async () => {
+    const { sut } = makeSut();
+    const fakeDate = {
+      body: {
+        email: "any@gmail.com",
+      },
+    };
+    expect((await sut.handle(fakeDate)).statusCode).toBe(400);
+    expect((await sut.handle(fakeDate)).body).toEqual({
+      error: "password not provided",
     });
   });
 });

238a6a1b4d624aed3cf30fab3508b5cf15e5782b
Lucca <luccazvps@gmail.com>
2022-12-29 11:06:44 -0300

    refactor: put new type in findByEmail

--- src/presentation/protocols/verify-email.ts
+++ src/presentation/protocols/verify-email.ts
@@ -1,5 +1,5 @@
 import { Account } from "@/domain/models/account";
 
 export interface findByEmail {
-  verify: (email: string) => Promise<Account>;
+  verify: (email: string) => Promise<Account | false>;
 }

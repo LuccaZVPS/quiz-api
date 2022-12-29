cf7b2180f7df6bdc8124882901a6feca7e5eccb4
Lucca <luccazvps@gmail.com>
2022-12-29 09:59:28 -0300

    feat: ensure auth controller return 400 if no email is provided

---
+++ src/presentation/controllers/auth.ts
@@ -0,0 +1,15 @@
+import { badRequest, ok } from "../helpers/http-helpers";
+import { Controller } from "../protocols/controller";
+import { httpRequest, httpResponse } from "../protocols/http";
+
+export class AuthController implements Controller {
+  async handle(response: httpRequest): Promise<httpResponse> {
+    const fields = ["email", "password"];
+    for (var field of fields) {
+      if (!response.body[field]) {
+        return badRequest(`${field} not provided`);
+      }
+    }
+    return ok();
+  }
+}

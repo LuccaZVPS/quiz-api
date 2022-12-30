5b195c5c9aecbe8391e9817de7696c5379d55ed4
Lucca <luccazvps@gmail.com>
2022-12-30 10:10:19 -0300

    feat: ensure emailVerify call FindByEmail with correct values

---
+++ src/data/useCases/email-verify.ts
@@ -0,0 +1,11 @@
+import { Account } from "@/domain/models/account";
+import { findByEmail } from "@/presentation/protocols/verify-email";
+import { findByEmail as findUserWithEmail } from "../protocols/findByEmail";
+
+export class EmailVerify implements findByEmail {
+  constructor(private findByEmail: findUserWithEmail) {}
+  async verify(email: string): Promise<false | Account> {
+    const account = await this.findByEmail.find(email);
+    return false;
+  }
+}

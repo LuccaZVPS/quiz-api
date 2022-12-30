f9e8b5b7b544fe61f63efe282a2c64ee48bb7d44
Lucca <luccazvps@gmail.com>
2022-12-30 10:25:42 -0300

    test: ensure email verify return the same value of  findByEmail

--- tests/data/email-verify.spec.ts
+++ tests/data/email-verify.spec.ts
@@ -27,13 +27,27 @@ describe("EmailVerify", () => {
 
   test("should throw if findByEmailThrows", async () => {
     const { emailVerify, findByEmailStub } = makeSut();
-    const spy = jest
-      .spyOn(findByEmailStub, "find")
-      .mockImplementationOnce(async () => {
-        throw new Error();
-      });
+    jest.spyOn(findByEmailStub, "find").mockImplementationOnce(async () => {
+      throw new Error();
+    });
     expect(async () => {
       await emailVerify.verify("any@gmail.com");
     }).rejects.toThrow();
+  });
+  test("should return an account if findByEmail returns an account", async () => {
+    const { emailVerify, findByEmailStub } = makeSut();
+    var account = await emailVerify.verify("any@gmail.com");
+    expect(account).toEqual({
+      name: "Lucca",
+      email: "any@gmail.com",
+      password: "12345678",
+      id: "a1sd1asd4",
+    });
+
+    jest.spyOn(findByEmailStub, "find").mockImplementationOnce(async () => {
+      return false;
+    });
+    account = await emailVerify.verify("any@gmail.com");
+    expect(account).toBe(false);
   });
 });

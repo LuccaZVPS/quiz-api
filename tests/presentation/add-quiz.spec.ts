e376850e08cbc3be65d572ec508deca446b87307
Lucca <luccazvps@gmail.com>
2023-01-04 10:43:05 -0300

    test: ensure AddQuizController return 400 if quiz is not provided

--- tests/presentation/add-quiz.spec.ts
+++ tests/presentation/add-quiz.spec.ts
@@ -1,4 +1,5 @@
 import { AddQuizController } from "../../src/presentation/controllers/add-quiz";
+import { validQuestions } from "./mocks/quiz";
 describe("AddQuizController", () => {
   const makeSut = () => {
     return {
@@ -10,5 +11,53 @@ describe("AddQuizController", () => {
     const response = await sut.handle({});
     expect(response.statusCode).toBe(400);
     expect(response.body).toEqual({ error: "body not provided" });
+  });
+  test("should return bad request if quiz name  is not provided", async () => {
+    const { sut } = makeSut();
+    const response = await sut.handle({
+      body: {
+        categories: ["javascript", "react"],
+        questions: validQuestions,
+        correct: 1,
+      },
+    });
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toEqual({ error: "quiz name not provided" });
+  });
+  test("should return bad request if quiz categories  is not provided", async () => {
+    const { sut } = makeSut();
+    const response = await sut.handle({
+      body: {
+        name: "fundamentos do React",
+        questions: validQuestions,
+        correct: 1,
+      },
+    });
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toEqual({ error: "quiz categories not provided" });
+  });
+  test("should return bad request if quiz categories  is not provided", async () => {
+    const { sut } = makeSut();
+    const response = await sut.handle({
+      body: {
+        name: "fundamentos do React",
+        categories: ["javascript", "react"],
+        correct: 1,
+      },
+    });
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toEqual({ error: "quiz questions not provided" });
+  });
+  test("should return bad request if quiz categories  is not provided", async () => {
+    const { sut } = makeSut();
+    const response = await sut.handle({
+      body: {
+        name: "fundamentos do React",
+        categories: ["javascript", "react"],
+        questions: validQuestions,
+      },
+    });
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toEqual({ error: "quiz correct not provided" });
   });
 });
---
+++ tests/presentation/mocks/quiz.ts
@@ -0,0 +1,39 @@
+export const validQuestions = [
+  {
+    title: "Para que serve o Map no javascript",
+    answers: [
+      "iterar uma lista",
+      "deletar um elemento",
+      "pegar o ultimo elemento de um array",
+      "apagar o primeiro elemento de um array",
+    ],
+  },
+
+  {
+    title: "Como o useEffect é utilizado",
+    answers: [
+      "iterar uma lista",
+      "deletar um elemento",
+      "pegar o ultimo elemento de um array",
+      "apagar o primeiro elemento de um array",
+    ],
+  },
+  {
+    title: "Porque usar o UseMemo",
+    answers: [
+      "iterar uma lista",
+      "deletar um elemento",
+      "pegar o ultimo elemento de um array",
+      "apagar o primeiro elemento de um array",
+    ],
+  },
+  {
+    title: "Quando usar o useCallback",
+    answers: [
+      "iterar uma lista",
+      "deletar um elemento",
+      "pegar o ultimo elemento de um array",
+      "apagar o primeiro elemento de um array",
+    ],
+  },
+];
